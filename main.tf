e3342e076be9c9b575714bd76dbb0c912b641e64
Karen Olson <karmarie@gmail.com>
2023-01-13 16:58:28 -0600

    Initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+/* resource "<PROVIDER>_<TYPE>" "<NAME"> {
+        [CONFIG ...]
+    }
+*/
+
+# step 1: configure the provider(s) you want to use
+provider "aws" {
+    region = "us-east-2"
+}
+
+
+resource "aws_instance" "example" {
+    # Amazon Machine Image that runs on the EC2 instance
+    # There are free and paid AMIs in the AWS Marketplace. 
+    # This AMI is free, but the IDs differ by region.
+    ami           = "ami-0fb653ca2d3203ac1"
+    # Type of EC2 Instance to run
+    # The EC2 Instance Types page lists all available options for CUP, memory, disk space, and networking capacity.
+    # This one has one virtual CPI, 1GB memory, and is on the AWS Free Tier
+    instance_type = "t2.micro"
+    tags = {
+        Name = "terraform-first-example"
+    }
+}
